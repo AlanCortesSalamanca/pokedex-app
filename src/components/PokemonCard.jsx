9c661639ad76453e556ae0f7a957fb7340824f1e
Acaset13 <alan13cor@gmail.com>
2026-04-08 01:17:23 -0600

    feat: stabilize card layout using flexbox with basic img

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,35 @@
+import {useEffect, useState} from "react";
+
 function PokemonCard({name, index}){
-    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() =>{
+        fetch(`http://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(res => res.json())
+        .then(data => setPokemon(data))
+        .catch(error => console.error(error));
+
+    }, [name]);
+
+    if(!pokemon) return <p>Cargando...</p>
 
     return(
         <div className="card">
-            <img src={imageUrl} alt={name} />
-            <p>{name}</p>
+            <div className="imgarea">
+                <img src={pokemon.sprites.front_default} alt={name} />
+            </div>
+            <div className="info">
+                <p className="id">#{pokemon.id}</p>
+                <p className="name">{pokemon.name}</p>
+            </div>
+            <div className="types">{
+                pokemon.types.map((type, index) => (
+                    <span key={index} className={`type ${type.type.name}`}>
+                        {type.type.name}
+                    </span>
+                ))
+                }
+            </div>
         </div>
     );
 }
