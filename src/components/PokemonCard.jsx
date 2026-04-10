ab302a0c82da39639f45b07b3f291365c8b6c03e
Acaset13 <alan13cor@gmail.com>
2026-04-10 01:18:44 -0600

    feat(ui): enhance search bar layout
    - fix input and button alignment using flex
    - implement floating suggestions dropdown (absolute position)
    - prevent layout shift from suggestions
    - improve overall pokedex-style UI

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,35 @@
 import {useEffect, useState} from "react";
 
-function PokemonCard({name, index}){
-    const [pokemon, setPokemon] = useState(null);
+function PokemonCard({id}){
+    const [pokemon, setPokemon] = useState(undefined);
     
-    useEffect(() =>{
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(res => res.json())
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(res => {
+            if (!res.ok) throw new Error("No encontrado");
+            return res.json();
+            })
         .then(data => setPokemon(data))
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.error(error);
+            setPokemon(null);
+        });
 
-    }, [name]);
+    }, [id]);
 
-    if(!pokemon) return <p>Cargando...</p>
+    if(pokemon == null) return null ;
 
-    const imageUrl = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/${String(pokemon.id).padStart(3, "0")}.png`;
+    const imageUrl = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/${String(id).padStart(3, "0")}.png`;
     
 
     return(
         <div className="card">
             <div className="imgarea">
-                <img src={imageUrl} alt={name} />
+                <img src={imageUrl} alt={id} />
             </div>
             <div className="info">
                 <p className="id">#{pokemon.id}</p>
-                <p className="name">{pokemon.name}</p>
+                <p className="name">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
             </div>
             <div className="types">{
                 pokemon.types.map((type, index) => (
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,44 +2,129 @@ import {useEffect,useState} from "react";
 import PokemonCard from "../components/PokemonCard";
 
 function Home(){
-    const [pokemons, setPokemons] = useState([]);
+    const [allPokemons, setAllPokemons] = useState([]);
     const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
+    const [visibleCount, setVisibleCount] = useState(20);
 
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+        fetch("https://pokeapi.co/api/v2/pokemon-species?limit=1100")
         .then(res => res.json())
-        .then(data => setPokemons(data.results))
-        .then(err => console.error(err));
+        .then(data => setAllPokemons(data.results))
+        .catch(err => console.error(err));
     }, []);
 
-    const filteredPokemons = pokemons.filter(pokemon =>
-        pokemon.name.toLowerCase().includes(search.toLowerCase()) ||
-        pokemon.url.includes(search)
-    );
+    const suggestions = allPokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(search.toLowerCase())
+    ).slice(0, 6);
+
+    const handleSearch = () => {
+        if (!search) {
+            setResults([]);
+            return;
+        }
+
+        const filtered = allPokemons.filter(pokemon => {
+            const id = pokemon.url.split("/")[6];
+            return(
+                pokemon.name.toLowerCase().includes(search.toLowerCase()) ||
+                id.includes(search)
+            );
+
+        });
+
+        if (filtered.length === 0){
+            alert("No se encontraron resultados");
+        }
+
+        setResults(filtered);
+        setVisibleCount(20);
+    };
+
+    const dataToShow = results.length > 0 ? results : allPokemons;
 
     return(
         <div>
             
-            <h2>
-                Lista de Pokémon
-            </h2>
+            <header className="filter-h">
+                <div className="filter-container">
 
-            <input
-                type="text"
-                placeholder="Buscar Pokemon..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
+                    <div className="filter-left">
+                        <label htmlFor="searchInput"> Nombre o número</label>
+
+                        <div className="search-row">
+                            <input 
+                                type="text"
+                                id="searchInput"
+                                placeholder="Ej: Pikachu o 255"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") handleSearch();
+                                }} 
+                            />
+                            
+
+                            <button onClick={handleSearch}>
+                                Buscar
+                            </button>
+                            
+                            {search && (
+                                <div className="suggestions">
+                                    {suggestions.map((p) => {
+                                        const id = p.url.split("/")[6];
+
+                                        return(
+                                            <div
+                                                key={id}
+                                                onClick={() => setSearch(p.name)}
+                                                className="suggestion-item"
+                                            >
+                                                {p.name}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            )}
+                        </div>
+                       
+
+                        <p className="subtitle">
+                            ¡Utiliza la busqueda avanzada para explorar Pokemon por tipo, debilidad, habilidad y mucho mas!
+                        </p>
+
+                    </div>
+
+                    <div className="filter-right">
+                        <div className="search-banner">
+                            <h2>
+                                Busca un pokémon por su nombre o utilizando su número en la Pokedex Nacional
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            
+            
+
 
             <div className="container">
-                {filteredPokemons.map((pokemon,index) =>(
-                    <PokemonCard
-                    key={index}
-                    name={pokemon.name}
-                    index={index}
+                {dataToShow.slice(0, visibleCount).map((pokemon) => {
+                    const id = pokemon.url.split("/")[6];          
+                    return(<
+                    PokemonCard 
+                    key={id}
+                    id={id}
                     />
-                ))}
+                    );
+                })}
             </div>
+
+            {visibleCount < dataToShow.length && (
+                <button onClick={() => setVisibleCount(prev => prev + 20)}>
+                    Mostrar Mas Pokémons
+                </button>
+            )}
 
         </div>
     );
